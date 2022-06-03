58c29ae79256e03bbea98f865e0de6d636cba0db
gogooma93 <99804202+gogooma93@users.noreply.github.com>
2022-06-03 16:41:49 +0900

    Update settings.py

--- coinRoot/coin/settings.py
+++ coinRoot/coin/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'coin.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'coin',
+        'NAME': '****',
         'USER': '****',
         'PASSWORD': '****',
         'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'PORT': '****',
     }
 }
 
